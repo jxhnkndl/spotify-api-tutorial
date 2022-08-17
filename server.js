10e60d6036eaf8cd0f6c3ac41a0a42b1d6466ad8
jxhnkndl <jkroyston@gmail.com>
2022-08-17 16:41:28 -0400

    configure dev environment

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 
+require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
 app.get('/', (req, res) => {
-  res.send('Hello Spotify')
+  res.send('Hello Spotify');
 });
 
 app.listen(PORT, () => {
